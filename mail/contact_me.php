fe56dab20f174d715fac5d7a9888ad00824e9f80
Rafael Delgado Martins <raziel_takato@hotmail.com>
2016-08-04 12:24:51 -0300

    conteúdo atualizado

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,32 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
+if(empty($_POST['name']) 		||
+  empty($_POST['email']) 		||
+  empty($_POST['phone']) 		||
+  empty($_POST['message'])	||
+!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+{
+  echo "No arguments Provided!";
+  return false;
+}
+
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$to="rafael.d.martins@hotmail.com";
+$from=strip_tags(htmlspecialchars($_POST['email']));
+$from_name=strip_tags(htmlspecialchars($_POST['name']));
+$headers="From: rdmar379@rdmartins.me\r\n";
+$headers.="MIME-Version: 1.0\r\n";
+$headers.="Content-Type: text/html; charset=UTF-8\r\n";
+$msg="<html>\r\n";
+$msg.="<body>\r\n";
+$msg.="<h2>$from_name</h2><br />\r\n";
+$msg.="$from<br />\r\n";
+$msg.="$phone\r\n";
+$msg.="<p>$message</p>\r\n";
+$msg.="</body>\r\n";
+$msg.="</html>\r\n";
+$subject="Contato: $from_name";
+
+mail($to, $subject, $msg, $headers);
 ?>
